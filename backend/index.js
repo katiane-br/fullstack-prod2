4b37ee3f9b7cb54d319e8092279cc83d272d57ca
jaubass <jaubass@gmail.com>
2023-11-05 14:21:46 +0100

    npm/nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World Jack X')
 })
 
-app.listen(3000)
+
+
+app.listen(3000, () => {
+  console.log('Servidor up en puerto 3000')
+})
