0aa1e0096b1a15916f9aad26dd61ac07260f8eb0
jaubass <jaubass@gmail.com>
2023-11-05 15:31:09 +0100

    conectarDB  db.js

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const conectarDB = async () => {
+    try {
+    } catch (error) {
+        console.log(`error: ${error.message}`);
+        process.exit(1);
+    }
+};
