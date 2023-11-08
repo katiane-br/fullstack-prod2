6cec68c9dd3d7a22944f9d2ce0c5e645b3867d76
jaubass <jaubass@gmail.com>
2023-11-08 20:36:16 +0100

    Routing semestres

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conectarDB from './config/db.js';
+import semestresRoutes from './routes/semestresRoutes.js'
 
 const app = express();
 dotenv.config();
 conectarDB();
 
-const PORT = process.env.PORT || 4000;
+// Routing
+app.get("/", (req, res) => {
+  res.send("Hola Mundo, by Jack X");
+});
 
-app.listen(3000, () => {
+app.use("/semestres", semestresRoutes);
+
+// Puerto escuchado
+const PORT = process.env.PORT || 4000;
+app.listen(4000, () => {
   console.log('Servidor up en puerto 4000')
 })
