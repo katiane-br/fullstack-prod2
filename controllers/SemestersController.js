b3d73b476cca59bf380325c7464c9878c65bc7f6
Xavimat <1102677+xavi-mat@users.noreply.github.com>
2023-11-08 16:16:20 +0100

    Controllers

--- controllers/SemestersController.js
+++ controllers/SemestersController.js
@@ -1,1 +1,25 @@
+import Semester from "../models/Semester.js";
 
+const SemestersController = {
+    getSemestersList: async () => {
+        return await Semester.find({});
+    },
+    getSemesterById: async id => {
+        return await Semester.findById(id)
+            .populate("subjects");
+    },
+    createSemester: async newSemester => {
+        return await Semester.create(newSemester);
+    },
+    updateSemester: async (id, updatedSemester) => {
+        return await Semester.findByIdAndUpdate(id, updatedSemester, {new: true});
+    },
+    deleteSemester: async id => {
+        return await Semester.findByIdAndDelete(id);
+    },
+};
+
+
+
+
+export default SemestersController;
