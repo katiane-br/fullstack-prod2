7a1a834c8249c6f8fdb2a527daaddd82ef2a9574
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-11-19 16:25:36 +0100

    details

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const DB_URI = process.env.DB_URI;
 const dbConnection = async () => {
   try {
     await connect(DB_URI);
-    console.log("🔗  DB successfully connected");
+    console.log("🔗 DB successfully connected");
   } catch (error) {
     console.error(error);
     throw new Error("Error connecting to DB");
